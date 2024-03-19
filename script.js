42094430b83fb8e6f63b75501bc23cac3990399c
sachin7con <86023048+sachin7con@users.noreply.github.com>
2024-03-19 15:42:48 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
-//your JS code here. If required.
+//SGN your JS code here. If required.
+let elem = document.getElementById("level");
+let count = 2;
+while(elem.parentNode.tagName.toLowerCase() !== 'html'){
+	count ++;
+	elem = elem.parentNode;
+	
+}
+
+
+alert(count);
